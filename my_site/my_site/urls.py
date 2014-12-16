050bcde3de96ef17a60b6e1001b8994b0ae417af
aguda <aguda@symbyo.com>
2014-12-16 18:55:11 +0200

    Tutorial 3
    Signed-off-by: aguda <aguda@symbyo.com>

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'my_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('poll.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- my_site/poll/views.py
+++ my_site/poll/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from poll.models import Question
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return HttpResponse("You're looking at question %s." % question.question_text)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
